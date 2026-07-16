fac88074e9c3bd51c229bde980097a18ce874373
Ck666-cpu <chingkeat0310@gmail.com>
2026-07-16 22:11:08 +0800

    modify main.py in worker file

--- worker/main.py
+++ worker/main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print("Python Worker 已启动，等待接收 RAG 任务...")
+    while True:
+        # 模拟一个一直运行的后台服务
+        time.sleep(10)
